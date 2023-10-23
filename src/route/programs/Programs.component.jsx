9555241ee986687e6ab15af6e0fc44f26859cdab
Regina Sirotkina <81709493+ReginaSi@users.noreply.github.com>
2023-10-22 18:03:58 -0600

    programs page
    first draft, don't hate me

--- src/route/programs/Programs.component.jsx
+++ src/route/programs/Programs.component.jsx
@@ -1,5 +1,90 @@
 const Programs = () => {
-  return <h1> Programs page </h1>;
+  return (
+      <div className="programs">
+        <h2>I know what you did last summer....</h2>
+
+
+        <div className="program-container">
+          <div className="program-content">
+            <h3>Software Development Diploma (2 years)</h3>
+            <p>
+              A two-year software development diploma program offers a comprehensive and focused education
+                in the field of software development. Over this period, students gain valuable skills and
+                knowledge, including programming languages, software architecture, web development,
+                and database management. With a strong emphasis on practical learning, students engage
+                in hands-on projects and collaborate on real-world software applications.
+                This intensive program equips graduates with the expertise needed to enter
+                the software development industry and build a successful career in fields such as web development,
+                mobile app development, and software engineering.
+            </p>
+          </div>
+
+            <div className="fees-details">
+                <p>Course code SDDW24</p>
+                <p style={{ fontWeight: 'bold' }}>Winter - 2024</p>
+              <p>Start date: January 10, 2024</p>
+              <p>End date: April 24, 2025</p>
+              <p>$9,254 domestic </p>
+                <p>$27,735 international</p>
+            </div>
+        </div>
+
+
+
+
+
+        <div className="program-container">
+            <div className="program-content">
+          <h3>Software Development Post Diploma (1 year)</h3>
+           <p>The Software Development Post Diploma program, spanning one year, offers an advanced
+               and specialized curriculum designed to build upon existing knowledge and skills in software development.
+               This intensive program focuses on equipping students with the expertise required to excel in the
+               ever-evolving software industry. Throughout the course, participants delve into advanced topics, honing their programming abilities, software architecture design, and project management skills. By the program's conclusion, graduates are well-prepared to take on pivotal roles within the software development realm,
+               demonstrating their ability to innovate, lead, and tackle complex software projects effectively.</p>
+
+            </div>
+
+            <div className="fees-details">
+                <p>Course code SPDW24</p>
+                <p style={{ fontWeight: 'bold' }}>Winter - 2024</p>
+                <p>Start date: January 10, 2024</p>
+                <p>End date: December 18, 2024</p>
+                <p>$9,254 domestic </p>
+                <p>$15,688 international</p>
+
+            </div>
+        </div>
+
+
+        <div className="program-container">
+            <div className="program-content">
+          <h3>Software Development Certificate (3-6 months)</h3>
+
+                <p>
+                    The Software Development Certificate program, with a duration ranging from 3 to 6 months,
+                    provides a concise yet comprehensive path to acquire essential software development skills.
+                    Tailored for those looking to quickly enter the tech industry, this program covers core programming
+                    languages, web development techniques, and database management. Students engage in practical,
+                    hands-on projects and gain the practical knowledge required to build software applications efficiently.
+                    This program is an ideal choice for individuals seeking a rapid entry into the software development field,
+                    enabling them to kickstart their careers or explore this dynamic industry in a short timeframe.
+                </p>
+
+            </div>
+
+            <div className="fees-details">
+                <p>Course code SDDF24</p>
+                <p style={{ fontWeight: 'bold' }}>Spring - 2024</p>
+                <p>Start date: May 05, 2024</p>
+                <p>End date: August 18, 2024</p>
+                <p>$2,750/5,340 domestic </p>
+                <p>Not available for international students</p>
+
+            </div>
+        </div>
+
+      </div>
+  );
 };
 
 export default Programs;
