9f99631b5fc3cedd9e1d66c78e732ce9acff0bf4
hugozaminian <hugozamian@hotmail.com>
2023-10-26 19:54:37 -0600

    created signup-page

--- src/route/sign-up/SignUp.component copy.jsx
+++ src/route/sign-up/SignUp.component copy.jsx
@@ -4,14 +4,19 @@ import TitlePage from "../../components/title-page/TitlePage.component";
 import { FormWrapper, CustomButton } from "./SignUp.styles";
 
 import { Form, Col, Row } from "react-bootstrap";
+import { admAddNewCourse } from "../../util/api/api";
+import {
+  capitalizeEachWord,
+  convertToAMPM,
+  getFormatDate,
+} from "../../util/general-functions/generalFunctions";
 
 const SignUp = () => {
   const [validated, setValidated] = useState(false);
-  const [countryCode, setCountryCode] = useState("+1"); // State variable for country code
+  const [phoneNumber, setPhoneNumber] = useState(""); // State variable to store the phone number
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -37,13 +42,16 @@ const SignUp = () => {
       //   class_size: form.classSize.value,
       // };
 
-      // admAddNewCourse(newCourseData);
-
-      const numericPhoneNumber = form.phone.value.replace(/\D/g, "");
-      const formattedPhoneNumber = `${countryCode} (${numericPhoneNumber.slice(
+      // Format the phone number as needed before sending it to the server
+      const formattedPhoneNumber = `+1 (${phoneNumber.slice(
         0,
         3
-      )}) ${numericPhoneNumber.slice(3, 6)}-${numericPhoneNumber.slice(6)}`;
+      )}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
+
+      // Now you can send the formattedPhoneNumber to the server or perform other actions
+      console.log(formattedPhoneNumber);
+
+      // admAddNewCourse(newCourseData);
     }
   };
 
@@ -52,7 +60,7 @@ const SignUp = () => {
       <TitlePage title="Registration form" />
 
       <FormWrapper noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="fname">
+        <Form.Group className="mb-3" controlId="name">
           <Form.Label style={{ color: "var(--color_font2)" }}>
             <strong>First Name:</strong>
           </Form.Label>
@@ -63,7 +71,7 @@ const SignUp = () => {
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="lname">
+        <Form.Group className="mb-3" controlId="name">
           <Form.Label style={{ color: "var(--color_font2)" }}>
             <strong>Last Name:</strong>
           </Form.Label>
@@ -74,41 +82,37 @@ const SignUp = () => {
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
 
-        <Row>
-          <Col lg={3}>
-            <Form.Group className="mb-3" controlId="countryCode">
-              <Form.Label style={{ color: "var(--color_font2)" }}>
-                <strong>Country Code:</strong>
-              </Form.Label>
-              <Form.Control
-                as="select"
-                value={countryCode}
-                onChange={(e) => setCountryCode(e.target.value)}>
-                <option value="+1">+1 (United States)</option>
-                <option value="+44">+44 (United Kingdom)</option>
-                <option value="+55">+55 (Brazil)</option>
-                {/* Add more country code options as needed */}
-              </Form.Control>
-            </Form.Group>
-          </Col>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label style={{ color: "var(--color_font2)" }}>
+            <strong>Email:</strong>
+          </Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Phone"
+            required
+            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter your phone.
+          </Form.Control.Feedback>
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
 
-          <Col lg={9}>
-            <Form.Group className="mb-3" controlId="phone">
-              <Form.Label style={{ color: "var(--color_font2)" }}>
-                <strong>Phone:</strong>
-              </Form.Label>
-              <Form.Control
-                type="tel"
-                placeholder="10 digit phone XXXXXXXXXX"
-                pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your phone number 10 digits.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Col>
-        </Row>
+        <Form.Group className="mb-3" controlId="phone">
+          <Form.Label style={{ color: "var(--color_font2)" }}>
+            <strong>Phone:</strong>
+          </Form.Label>
+          <Form.Control
+            type="tel"
+            placeholder="Phone +1 (XXX) XXX-XXXX"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value.replace(/\D/g, ""))} // Allow only digits
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter your phone: +1 (XXX) XXX-XXXX.
+          </Form.Control.Feedback>
+        </Form.Group>
 
         <Form.Group className="mb-3" controlId="birthday">
           <Form.Label style={{ color: "var(--color_font2)" }}>
@@ -171,7 +175,7 @@ const SignUp = () => {
           <Form.Label style={{ color: "var(--color_font2)" }}>
             <strong>Password:</strong>
           </Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control type="text" placeholder="Password" required />
           <Form.Control.Feedback type="invalid">
             Please enter your password.
           </Form.Control.Feedback>
