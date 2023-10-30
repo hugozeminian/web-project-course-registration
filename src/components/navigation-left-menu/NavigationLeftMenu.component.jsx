8d91bce63900c7e5de40f03b7aa3d9de863aad91
hugozaminian <hugozamian@hotmail.com>
2023-10-30 01:33:50 -0600

    changed place of button login and logout

--- src/components/navigation-left-menu/NavigationLeftMenu.component.jsx
+++ src/components/navigation-left-menu/NavigationLeftMenu.component.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import {CurtainMenuContainerBackground, CurtainMenuContainer, MenuItems, MenuItem, MenuLink, Icon} from "./NavigationLeftMenu.styles"
-import { faFileLines, faArrowRightToBracket, faCubes, faPen, faInfo, } from "@fortawesome/free-solid-svg-icons";
-import { getAuthenticatedUser, logout } from "../../util/api/api";
+import { faFileLines, faCubes, faPen, faInfo, } from "@fortawesome/free-solid-svg-icons";
 
 const NavigationLeftMenu = ({ open, toggleMenu }) => {
-
-  const authenticatedUser = getAuthenticatedUser() || {}
-  const isAuthenticated = authenticatedUser.isAuthenticated
-
-  const handleLogout = () => {
-    logout()
-  }
 
   return (
     <>
@@ -18,9 +10,6 @@ const NavigationLeftMenu = ({ open, toggleMenu }) => {
         <CurtainMenuContainer open={open}>
           <MenuItems>
             <MenuItem> <MenuLink to='/'> <Icon icon={faFileLines} className="fa-lg" /> HOME </MenuLink></MenuItem>
-            {isAuthenticated ? 
-              (<MenuItem> <MenuLink to='/login' onClick={handleLogout}> <Icon icon={faArrowRightToBracket} className="fa-lg" rotation={180} /> LOGOUT </MenuLink> </MenuItem>) : 
-              (<MenuItem> <MenuLink to='/login'> <Icon icon={faArrowRightToBracket} className="fa-lg" /> LOGIN </MenuLink> </MenuItem>)}
             <MenuItem> <MenuLink to='/programs'> <Icon icon={faCubes} className="fa-lg" /> PROGRAMS </MenuLink> </MenuItem>
             <MenuItem> <MenuLink to='/courses'> <Icon icon={faPen} className="fa-lg" /> COURSES </MenuLink> </MenuItem>
             <MenuItem> <MenuLink to='/about'> <Icon icon={faInfo} className="fa-lg" /> ABOUT </MenuLink> </MenuItem>
