ec7e2acf1bf5bce4381809326b5203951beafd97
Fabio Weck <fabio.weck@gmail.com>
2023-11-20 19:40:48 -0700

    programs list route added/middleware improvements

--- server_side/services/addUser.js
+++ server_side/services/addUser.js
@@ -15,7 +15,14 @@ export const AddUser = async (data) =>
         const user = {
             userName: data.userName,
             password: hash,
-            accessLevel: data.accessLevel
+            accessLevel: data.accessLevel,
+            firstName: data.firstName,
+            middleName: data.middleName,
+            lastName: data.lastName,
+            phone: data.phone,
+            email: data.email,
+            programID: data.programID,
+            dateOfBirth: data.dateOfBirth
         }
 
         try{
@@ -23,7 +30,7 @@ export const AddUser = async (data) =>
             //Creates login but not student or admin
             await sql.connect(config);
     
-            const query = 'INSERT INTO Login VALUES (@userName, @password, @accessLevel)';
+            const queryLogin = 'INSERT INTO Login VALUES (@userName, @password, @accessLevel)';
     
             const request = new sql.Request();
     
@@ -31,13 +38,59 @@ export const AddUser = async (data) =>
             request.input('password', sql.NVarChar, user.password);
             request.input('accessLevel', sql.Int, user.accessLevel);
             
-            const result = await request.query(query);
-    
-            return result;
-    
+            await request.query(queryLogin);
+
+
+            if(user.accessLevel === 1)
+            {
+                const queryStudent = `INSERT INTO Student VALUES 
+                                        (@userName, 
+                                        @firstName,
+                                        @middleName,
+                                        @lastName,
+                                        @phone,
+                                        @email,
+                                        @programID, 
+                                        @dateOfBirth)`;
+                
+                request.input('firstName', sql.NVarChar, user.firstName);
+                request.input('middleName', sql.NVarChar, user.middleName);
+                request.input('lastName', sql.NVarChar, user.lastName);
+                request.input('phone', sql.NVarChar, user.phone);
+                request.input('email', sql.NVarChar, user.email);
+                request.input('programID', sql.Int, user.programID);
+                request.input('dateOfBirth', sql.Date, user.dateOfBirth);
+
+                await request.query(queryStudent);
+
+            }
+            
+            if(user.accessLevel === 99)
+            {
+                const queryAdmin = `INSERT INTO Admin VALUES 
+                        (@userName, 
+                         @firstName,
+                         @middleName,
+                         @lastName,
+                         @phone,
+                         @email, 
+                         @dateOfBirth)`;
+                
+                request.input('firstName', sql.NVarChar, user.firstName);
+                request.input('middleName', sql.NVarChar, user.middleName);
+                request.input('lastName', sql.NVarChar, user.middleName);
+                request.input('phone', sql.NVarChar, user.phone);
+                request.input('email', sql.NVarChar, user.email);
+                request.input('dateOfBirth', sql.Date, user.dateOfBirth);
+
+                await request.query(queryAdmin);
+                
+            }
+
         }
         catch(err){
-            console.error('Error reading data:', err);
+            console.log("Unable to perform queries: " + err);
+            throw err;
         }
         finally{
             await sql.close();
@@ -45,6 +98,10 @@ export const AddUser = async (data) =>
 
     })
     .catch(err => {
-        console.error('Error while hashing password:', err);
+        console.log("Unable to hash the password: " + err);
+        throw err;
     });
+
+    return true;
+
 }
--- server_side/services/readCourses.js
+++ server_side/services/readCourses.js
@@ -1,14 +1,35 @@
 import sql from 'mssql';
 import {config} from './config.js';
 
-export const readData = async () =>
+export const ReadCourses = async () =>
 {
 
     try{
 
         await sql.connect(config);
 
-        const query = 'SELECT * FROM Course';
+        const query = 
+        `SELECT c.CourseCode,
+                c.Section,
+                c.Name,
+                c.Description,
+                c.Year,
+                t.Term,
+                c.Days,
+                c.Hours,
+                c.StartDate,
+                c.EndDate,
+                c.CampusID,
+                c.Room,
+                c.DomesticFees,
+                c.InternationalFees,
+                c.SeatsAvailable,
+                c.ClassSize,
+                c.DeliveryMode
+        FROM Course c
+        JOIN Term t
+        ON c.TermID = t.TermID
+        `;
 
         const request = new sql.Request();
 
